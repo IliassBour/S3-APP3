8360a064578bb8cbaa8e78b67ee533daefd57190
IliassB <iliass.bourabaa@usherbrooke.ca>
2020-10-26 14:08:00 -0400

    Modification LiaisonDeDonnes
    J'ai modifier la classe pour permettre la demande d'un nouveau fichier pour le sauvegarder dans le serveur

--- src/LiaisonDeDonnees.java
+++ src/LiaisonDeDonnees.java
@@ -1,9 +1,6 @@
 import java.io.*;
 import java.math.BigInteger;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.zip.CRC32;
@@ -63,6 +60,10 @@ public class LiaisonDeDonnees extends Thread implements Couche {
                     paquetsTransmisPerdus++;
                     Envoi(message);
                     break;
+
+                case "ProchainFichierServeur":
+                    ProchainFichier();
+                    break;
             }
         }
         catch (IOException ioe){
@@ -119,69 +120,71 @@ public class LiaisonDeDonnees extends Thread implements Couche {
         System.out.println("Wait");
         socket.receive(packet); //Attend le reception d'un message
         System.out.println("Received");
+        this.socket.close();
         this.paquetsRecus++;
 
-        //Création du CRC de vérification à partir du message
         byte[] message = packet.getData();
 
-        int finCRC = 0;
-        for(int i = 255; i > 0; i--){
-            if(message[i] != 0){
-                finCRC = i;
-                break;
+        if(verificationVide(message)) {
+            prochain.Handle("LireFichier", null);
+        } else {
+            //Création du CRC de vérification à partir du message
+            int finCRC = 0;
+            for (int i = 255; i > 0; i--) {
+                if (message[i] != 0) {
+                    finCRC = i;
+                    break;
+                }
             }
-        }
 
-        System.out.println(new String(message));
+            System.out.println(new String(message));
 
-        System.out.println("FinCRC: "+finCRC);
+            System.out.println("FinCRC: " + finCRC);
 
-        System.out.print(String.format("%8s", Integer.toBinaryString(message[finCRC-3] & 0xFF)).replace(' ', '0'));
-        System.out.print(' ');
-        System.out.print(String.format("%8s", Integer.toBinaryString(message[finCRC-2] & 0xFF)).replace(' ', '0'));
-        System.out.print(' ');
-        System.out.print(String.format("%8s", Integer.toBinaryString(message[finCRC-1] & 0xFF)).replace(' ', '0'));
-        System.out.print(' ');
-        System.out.println(String.format("%8s", Integer.toBinaryString(message[finCRC] & 0xFF)).replace(' ', '0'));
+            System.out.print(String.format("%8s", Integer.toBinaryString(message[finCRC - 3] & 0xFF)).replace(' ', '0'));
+            System.out.print(' ');
+            System.out.print(String.format("%8s", Integer.toBinaryString(message[finCRC - 2] & 0xFF)).replace(' ', '0'));
+            System.out.print(' ');
+            System.out.print(String.format("%8s", Integer.toBinaryString(message[finCRC - 1] & 0xFF)).replace(' ', '0'));
+            System.out.print(' ');
+            System.out.println(String.format("%8s", Integer.toBinaryString(message[finCRC] & 0xFF)).replace(' ', '0'));
 
-        System.out.println("Message length: "+message.length);
+            System.out.println("Message length: " + message.length);
 
-        CRC32 crcVerif = new CRC32();
-        crcVerif.update(message, 0, message.length-(message.length-finCRC)-4);
+            CRC32 crcVerif = new CRC32();
+            crcVerif.update(message, 0, message.length - (message.length - finCRC) - 4);
 
-        //Prise du CRC inclut dans le message
-        byte[] messageCRCBytes = {message[finCRC-3], message[finCRC-2], message[finCRC-1], message[finCRC]};
-        Long messageCRCValue = new BigInteger(messageCRCBytes).longValue();
+            //Prise du CRC inclut dans le message
+            byte[] messageCRCBytes = {message[finCRC - 3], message[finCRC - 2], message[finCRC - 1], message[finCRC]};
+            Long messageCRCValue = new BigInteger(messageCRCBytes).longValue();
 
-        //Erreur CRC
-        if(false/*messageCRCValue != crcVerif.getValue()*/){
-            this.paquetsRecusErreurCRC++;
-            Log("ErreurCRC");
-            System.out.println("Erreur");
-            System.out.println("messageCRCValue: "+messageCRCValue);
-            System.out.println("crcVerif value: "+crcVerif.getValue());
+            //Erreur CRC
+            if (false/*messageCRCValue != crcVerif.getValue()*/) {
+                this.paquetsRecusErreurCRC++;
+                Log("ErreurCRC");
+                System.out.println("Erreur");
+                System.out.println("messageCRCValue: " + messageCRCValue);
+                System.out.println("crcVerif value: " + crcVerif.getValue());
 
-            //Send Error to Transport
-            //Retrait du CRC du message
-            byte[] messageToPass = Arrays.copyOfRange(message, 0, message.length-4);
+                //Send Error to Transport
+                //Retrait du CRC du message
+                byte[] messageToPass = Arrays.copyOfRange(message, 0, message.length - 4);
 
-            //Envoi du message dans Transport
-            this.prochain.Handle("ErreurCRC", messageToPass);
+                //Envoi du message dans Transport
+                this.prochain.Handle("ErreurCRC", messageToPass);
 
-        }
-        //Réussite CRC
-        else{
-            Log("Recu");
-            System.out.println("Réussite");
+            }
+            //Réussite CRC
+            else {
+                Log("Recu");
+                System.out.println("Réussite");
 
-            //Retrait du CRC du message
-            byte[] messageToPass = Arrays.copyOfRange(message, 0, message.length-4);
+                //Retrait du CRC du message
+                byte[] messageToPass = Arrays.copyOfRange(message, 0, message.length - 4);
 
-            //Fermer socket
-            this.socket.close();
-
-            //Envoi du message dans Transport
-            this.prochain.Handle("Recu", messageToPass);
+                //Envoi du message dans Transport
+                this.prochain.Handle("Recu", messageToPass);
+            }
         }
     }
 
@@ -224,6 +227,31 @@ public class LiaisonDeDonnees extends Thread implements Couche {
             }
             writer.close();
         }
+    }
+
+    private void ProchainFichier() throws IOException {
+        InetAddress address = InetAddress.getByName(this.adresseIP);
+        byte[] messageVide = new byte[256];
+        DatagramPacket packet = new DatagramPacket(messageVide, messageVide.length, address, this.portSocket);
+        this.socket.send(packet);
+        Log("Envoi");
+        this.paquetsTransmis++;
+
+        //Écouter pour réponse
+        Handle("Recu", null);
+    }
+
+    private boolean verificationVide(byte[] message) {
+        boolean verif = true;
+
+        for(int index = 0; index < message.length; index++) {
+            if(message[index] != 0) {
+                verif = false;
+                break;
+            }
+        }
+
+        return verif;
     }
 
     public void run(){
